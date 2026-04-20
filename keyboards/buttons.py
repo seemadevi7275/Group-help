53a096eb1f16d56c179daf064368d9c53a2917a8
seemadevi7275 <seemadevi727534@gmail.com>
2026-04-20 18:40:24 +0530

    Update buttons.py

--- keyboards/buttons.py
+++ keyboards/buttons.py
@@ -1,1 +1,8 @@
 
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
+def main_panel():
+    return InlineKeyboardMarkup([
+        [InlineKeyboardButton("⚙️ Settings", callback_data="settings")],
+        [InlineKeyboardButton("📊 Stats", callback_data="stats")]
+    ])
