a790568b6283bcef6aef9d630ba8bd1a80ebf179
seemadevi7275 <seemadevi727534@gmail.com>
2026-04-20 18:41:16 +0530

    Update panel.py

--- handlers/panel.py
+++ handlers/panel.py
@@ -1,1 +1,10 @@
 
+from telegram import Update
+from telegram.ext import ContextTypes
+from keyboards.buttons import main_panel
+
+async def panel(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🎛 Control Panel",
+        reply_markup=main_panel()
+    )
