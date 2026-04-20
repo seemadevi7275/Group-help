cce0016cca564a31689676a0e55b17083ac4162e
seemadevi7275 <seemadevi727534@gmail.com>
2026-04-20 18:35:37 +0530

    Update anti_raid.py

--- services/anti_raid.py
+++ services/anti_raid.py
@@ -1,1 +1,14 @@
 
+from collections import deque
+import time
+
+joins = deque()
+
+def raid_detected():
+    now = time.time()
+    while joins and now - joins[0] > 10:
+        joins.popleft()
+
+    joins.append(now)
+
+    return len(joins) > 10  # 10 joins in 10 sec = raid
