200ec7c7d9da920eaaa57f28d497e14e756fa5ec
seemadevi7275 <seemadevi727534@gmail.com>
2026-04-20 18:33:11 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,13 @@
 
+roles = db["roles"]
+
+async def set_role(user_id, role):
+    await roles.update_one(
+        {"user_id": user_id},
+        {"$set": {"role": role}},
+        upsert=True
+    )
+
+async def get_role(user_id):
+    data = await roles.find_one({"user_id": user_id})
+    return data["role"] if data else "user"
