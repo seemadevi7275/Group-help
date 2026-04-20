fd9f8bc06c3c9b96cc73a1810f5b829c7d9cf86a
seemadevi7275 <seemadevi727534@gmail.com>
2026-04-20 18:34:16 +0530

    Update decorators.py

--- Core/decorators.py
+++ Core/decorators.py
@@ -1,1 +1,18 @@
+from functools import wraps
+from core.permissions import is_admin
+from db import get_role
 
+def admin_only(func):
+    @wraps(func)
+    async def wrapper(update, context):
+        if await is_admin(update, context):
+            return await func(update, context)
+    return wrapper
+
+def mod_only(func):
+    @wraps(func)
+    async def wrapper(update, context):
+        role = await get_role(update.effective_user.id)
+        if role in ["mod", "admin"]:
+            return await func(update, context)
+    return wrapper
